d00d123ac67a0804980df6e0bc358ebdce003445
William Fairleigh <wdf1999@tamu.edu>
2020-03-06 11:34:01 -0600

    move default work into function

--- Questions.java
+++ Questions.java
@@ -1,23 +1,64 @@
 import java.util.Formatter;
+import java.util.Locale;
+import java.lang.StringBuilder;
 
 public class Questions {
-    void normal(Formatter formated) {
+    static void normal(Formatter formated, dbConnect conn, String[] tables, String[] columns, String joinBy, 
+            int col1, String season) {
+
+        String query = conn.makeQuery(tables, columns, joinBy, col1, season);
+
+        // send the query to the database
+        String[] data = conn.sendQuery(query, columns);
+
+        // split the data into a more usable format
+        String[][] splitData = new String[columns.length][];
+
+        for (int i = 0; i < columns.length; i++) {
+            splitData[i] = data[i].split("\n");
+        }
+
+        // find largets string
+        int[] maxlengths = new int[columns.length];
+
+        for (int i = 0; i < columns.length; i++) {
+            maxlengths[i] = columns[i].length();
+        }
+
+        for (int i = 0; i < splitData.length; i++) {
+            for (int j = 0; j < splitData[i].length; j++) {
+                maxlengths[i] = Math.max(splitData[i][j].length(), maxlengths[i]);
+            }
+        }
+
+        // Start formatting the output in an even way
+        formated.format("Table 1:%20s\tTable2:%20s\n", tables[0], tables[1]);
+        for (int i = 0; i < columns.length; i++) {
+            formated.format("%-" + maxlengths[i] + "s%2s|%2s", columns[i], " ", " ");
+        }
+        formated.format("\n");
+
+        for (int i = 0; i < splitData[0].length; i++) {
+            for (int j = 0; j < columns.length; j++) {
+                formated.format("%-" + maxlengths[j] + "s%2s|%2s", splitData[j][i], " ", " ");
+            }
+            formated.format("\n");
+        }
+    }
+
+    static void q1(Formatter formated) {
 
     }
 
-    void q1(Formatter formated) {
+    static void q2(Formatter formated) {
 
     }
 
-    void q2(Formatter formated) {
+    static void q3(Formatter formated) {
 
     }
 
-    void q3(Formatter formated) {
-
-    }
-
-    void q4(Formatter formated) {
+    static void q4(Formatter formated) {
 
     }
 }
--- dbGuiV2.java
+++ dbGuiV2.java
@@ -733,47 +733,12 @@ public class dbGuiV2 extends javax.swing.JFrame {
         tables[0] = DefaultTableBox1.getSelectedItem().toString();
         tables[1] = DefaultTableBox2.getSelectedItem().toString();
 
-        query = conn.makeQuery(tables, columns, joinBy, col1.length, DefaultSeasonBox.getSelectedItem().toString());
-
-        // send the query to the database
-        String[] data = conn.sendQuery(query, columns);
-
-        // split the data into a more usable format
-        String[][] splitData = new String[columns.length][];
-
-        for (int i = 0; i < columns.length; i++) {
-            splitData[i] = data[i].split("\n");
-        }
-
-        // find largets string
-        int[] maxlengths = new int[columns.length];
-
-        for (int i = 0; i < columns.length; i++) {
-            maxlengths[i] = columns[i].length();
-        }
-
-        for (int i = 0; i < splitData.length; i++) {
-            for (int j = 0; j < splitData[i].length; j++) {
-                maxlengths[i] = Math.max(splitData[i][j].length(), maxlengths[i]);
-            }
-        }
-
         // Start formatting the output in an even way
         StringBuilder output = new StringBuilder();
         Formatter formated = new Formatter(output, Locale.US);
 
-        formated.format("Table 1:%20s\tTable2:%20s\n", tables[0], tables[1]);
-        for (int i = 0; i < columns.length; i++) {
-            formated.format("%-" + maxlengths[i] + "s%2s|%2s", columns[i], " ", " ");
-        }
-        formated.format("\n");
-
-        for (int i = 0; i < splitData[0].length; i++) {
-            for (int j = 0; j < columns.length; j++) {
-                formated.format("%-" + maxlengths[j] + "s%2s|%2s", splitData[j][i], " ", " ");
-            }
-            formated.format("\n");
-        }
+        Questions.normal(formated, conn, tables, columns, joinBy, col1.length, 
+                DefaultSeasonBox.getSelectedItem().toString());
 
         formated.close();
         // end formating and output
