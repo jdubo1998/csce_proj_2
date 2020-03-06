265b58726b18a132c312e9c8b1deb47db0cb812f
William Fairleigh <wdf1999@tamu.edu>
2020-03-05 18:59:24 -0600

    Create Questions.java

---
+++ Questions.java
@@ -0,0 +1,23 @@
+import java.util.Formatter;
+
+public class Questions {
+    void normal(Formatter formated) {
+
+    }
+
+    void q1(Formatter formated) {
+
+    }
+
+    void q2(Formatter formated) {
+
+    }
+
+    void q3(Formatter formated) {
+
+    }
+
+    void q4(Formatter formated) {
+
+    }
+}
